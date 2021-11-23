ebbc46694a4d440f82e34357d66dd2ff8e1be5f2
Philipp0110 <philli98@gmx.at>
2021-11-24 00:09:59 +0100

    added conan to the project to install dependencies

---
+++ cmake-build-debug/CMakeFiles/network-monitor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network-monitor.dir/src/main.cpp.obj"
+  "network-monitor.exe"
+  "network-monitor.exe.manifest"
+  "network-monitor.lib"
+  "network-monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network-monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
